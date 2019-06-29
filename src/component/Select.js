5e723db9deb10e1bf0e7f661a2d8111dc617c49f
59070178 <59070178@kmitl.ac.th>
2019-06-29 18:38:44 +0700

    Update Select.js

--- src/component/Select.js
+++ src/component/Select.js
@@ -4,22 +4,37 @@ import React from 'react';
 
 class Select extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {value: "2"};
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+ 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
   
   render() {
+    const { value } = this.state;
+
+
     return (
 
 
 <form>
         <label className="radio-inline">
-          <input type="radio" name="optradio" defaultChecked />A
+          <input type="radio" name="optradio" defaultValue="2" defaultChecked onChange={this.handleChange}/>A
         </label>
         <label className="radio-inline">
-          <input type="radio" name="optradio" />B
+          <input type="radio" name="optradio" defaultValue="1" onChange={this.handleChange}/>B
         </label>
         <label className="radio-inline">
-          <input type="radio" name="optradio" />X
+          <input type="radio" name="optradio" defaultValue="0" onChange={this.handleChange}/>X
         </label>
+
+        <h2>Rating from state: {value}</h2>
+
       </form>
 
 
